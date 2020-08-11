20ca3946b910fed65912d597ad90486af156dc30
lars-m <lam@cphbusiness.dk>
2020-08-11 11:45:48 +0200

    minor

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
   titleShort: `Dat3`,
   idLocalStorage: "fullstackFall2020",
   classes: [
-    { value: 'd', label: 'D-class', backgroundColor: "#295683" },
-    { value: 'e', label: 'E-class', backgroundColor: "darkgreen" }],
+    { value: 'd', label: 'D-Class', backgroundColor: "#295683" },
+    { value: 'e', label: 'E-Class', backgroundColor: "darkgreen" }],
   showWeekInfoForEachDayInWeek: false,
   timeEdit: "https://cloud.timeedit.net/cphbusiness/web/student",
   topMenu: [
